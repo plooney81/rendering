a6ae603875b0bb1c8d8e3aa271378f50f236db10
Pete Looney <petelooney81@gmail.com>
2020-09-10 16:27:58 -0500

    got solution to render tweets

--- renderTweets.js
+++ renderTweets.js
@@ -1,8 +1,63 @@
 
 function renderTweets(tweets) {
+    let letsTweet = '';
+    let checkCircle;
+    for (let index = 0; index < tweets.length; index++){
+        
+        // check circle logic
+        if (tweets[index].user.isVerified){
+            checkCircle = "width: 8px; height: auto;"
+        } else{
+            checkCircle = "display: none;"
+        }
+
+        letsTweet += `
+        <div class="tweet m-5" style="background-color: rgb(230, 230, 230)">
+            <div class="row align-items-start">
+                <img class="col-1" src="${tweets[index].user.profilePic}">
+                <div class="col-4">
+                    <div class="row align-items-center">
+                        <p class="font-weight-bold" style="margin: 0;">${tweets[index].user.username}</p>
+                        <img src="./twitterIcons/check-circle.svg" style=${checkCircle}>
+                    </div>
+                    <div class="row align-items-center">
+                        ${tweets[index].user.handle}
+                    </div>
+                </div>
+            </div>
+            <div class="row align-items-start">
+                <div class="col-6">
+                    <p class="font-weight-bold " style="margin: 0; ">${tweets[index].text}</p>
+                    <hr class="">
+                </div>
+            </div>
+            <div class="row ml-1 justify-content-start">
+                <div class="col-2">
+                    <div class="row  mr-3 align-items-center">
+                        <img src="./twitterIcons/message-circle.svg">
+                        <p style="margin: 0; ">${tweets[index].replies}</p>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="row  mr-3 align-items-center">
+                        <img src="./twitterIcons/repeat.svg">
+                        <p style="margin: 0; ">${tweets[index].retweets}</p>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="row  mr-3 align-items-center">
+                        <img src="./twitterIcons/heart.svg">
+                        <p style="margin: 0; ">${tweets[index].likes}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+    }
+    
     return `
-        <div class="text-center mt-5">
-            <code>${JSON.stringify(tweets)}</code>
+        <div class="mt-5">
+            ${letsTweet}
         </div>
     `
 }
