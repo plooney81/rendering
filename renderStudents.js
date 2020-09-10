bd0c7147bea90f61998eaf4d483ef16c72231f6f
Pete Looney <petelooney81@gmail.com>
2020-09-10 13:51:31 -0500

    students completed

--- renderStudents.js
+++ renderStudents.js
@@ -1,10 +1,32 @@
 
 function renderStudents(students) {
+    let names = '';
+        for (let index = 0; index < students.length; index++){
+            present = '';
+            color = '';
+            if (students[index].isPresent){
+                present = 'Present';
+                color = 'green';
+            } else {
+                present = 'Absent';
+                color = 'red';
+            }
+            names += `
+            <div class="d-flex flex-column border border-dark" style="background-color: ${color}">
+                <div class="h6">${students[index].name}</div>
+                <div class="h7">${present}</div>
+            </div>
+            `
+        }
+    
     return `
-        <div class="text-center mt-5">
-            <code>${JSON.stringify(students)}</code>
+        <div class="text-center mt-5" style="width: 500px; height: 500px;">
+            <div class="rollCall" style="width: 100px; height: 100px;">
+                <p class="h1">Roll Call!</p>
+                ${names}
+            </div>
         </div>
-    `
+    `;
 }
 
 function students() {
@@ -31,7 +53,7 @@ function students() {
             name: "Krissy",
             isPresent: false
         }
-    ]
+    ];
 
     content.innerHTML = renderStudents(studentsAbstraction);
-}
+};
