c1f0fcabb01205b4dd91a719d8d9bf68935ea373
Pete Looney <petelooney81@gmail.com>
2020-09-10 13:04:15 -0500

    completed the render nametags exercise

--- renderNametags.js
+++ renderNametags.js
@@ -1,8 +1,17 @@
 
 function renderNametags(nametags) {
+    let names = '';
+    for (let index = 0; index < nametags.length; index++){
+        names += `
+            <div class="${nametags[index]}" style="display: flex; flex-direction: column; justify-content: center; align-items: center; margin-bottom: 10px;">
+                <h7 style="color: white; background-color: blue; height: 35px; width: 200px; border-left: 1px solid black; border-right: 1px solid black; border-top: 1px solid black;">Hello my name is:</h7>
+                <p style="width:200px; height: 50px; color: black; border-left: 1px solid black; border-right: 1px solid black; border-bottom: 1px solid black;">${nametags[index]}</p>
+            </div>
+        `
+    }
     return `
         <div class="text-center mt-5">
-            <code>${JSON.stringify(nametags)}</code>
+            ${names}
         </div>
     `
 }
